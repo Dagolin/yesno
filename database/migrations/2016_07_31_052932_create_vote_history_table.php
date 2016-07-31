894c0157e08e23a6c688e8a8fc6f582fbba3b44c
Dago Lin <riderfish@gmail.com>
2016-07-31 13:33:02 +0800

    DB

---
+++ database/migrations/2016_07_31_052932_create_vote_history_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateVoteHistoryTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('vote_history', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('fingerprint');
+            $table->string('answer', 10);
+            $table->integer('vote_id');
+            $table->integer('user_id');
+            $table->timestamps();
+
+            $table->foreign('vot_id')->references('id')->on('votes')->onDelete('set null');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('vote_history');
+    }
+}
